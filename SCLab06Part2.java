c9b2a95e69e9fa6bb6888771b5615ba841fd2bf5
mranon0007 <mranon0007@gmail.com>
2018-03-23 21:50:23 +0500

    initial

---
+++ SCLab06Part2.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sclab06part2;
+import java.sql.*;
+import java.util.*;
+
+/**
+ *
+ * @author Yousuf Khan
+ */
+public class SCLab06Part2 {
+
+  
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+       
+        
+       
+    }
+    
+  
+    
+   
+    
+   
+}
