0136c3eec51ef756292eee8ebd9ce661066b1e52
mranon0007 <mranon0007@gmail.com>
2018-03-23 21:51:41 +0500

    Created Functions

--- SCLab06Part2.java
+++ SCLab06Part2.java
@@ -35,9 +35,75 @@ public class SCLab06Part2 {
        
     }
     
-  
+    public static void printAllStudents() throws SQLException {
+        
+        //Creating query
+        st = conn.createStatement();
+        query = "Select * from Students";
+        rs = st.executeQuery(query);
+
+        //Printing Results.
+        System.out.println("\n\n******Printing Students");
+        while(rs.next()) {
+            String name = rs.getString("Name");
+            String class_ = rs.getString("Class");
+            String regno = rs.getString("RegNo");
+            String contact = rs.getString("Contact");
+            String section = rs.getString("Section");
+            String address = rs.getString("Address");
+
+            System.out.println(
+                    "RegNo: " + regno + " " +
+                    "Name: " + name + " " +
+                    "Class: " + class_ + " " +
+                    "Section: " + section + " " +
+                    "Contact: " + contact + " " +
+                    "Address: " + address
+            );
+        }
+        System.out.println("************\n");
+        
+    }
     
-   
+    public static void deleteRecord(int id) throws SQLException {
+        
+        //Creating query
+        st = conn.createStatement();
+        query = "DELETE FROM `university`.`students` WHERE `RegNo`='"+id+"';";
+        rs = st.executeQuery(query);
+
+        //Printing Results.
+        System.out.println("\nDeleted Record.");
+        
+    }
     
-   
+    public static void searchRecord(int id) throws SQLException {
+        
+        //Creating query
+        st = conn.createStatement();
+        query = "Select * from Students where `RegNo`='"+id+"';";
+        rs = st.executeQuery(query);
+
+        //Printing Results.
+        System.out.println("\n\n******Printing Student");
+        while(rs.next()) {
+            String name = rs.getString("Name");
+            String class_ = rs.getString("Class");
+            String regno = rs.getString("RegNo");
+            String contact = rs.getString("Contact");
+            String section = rs.getString("Section");
+            String address = rs.getString("Address");
+
+            System.out.println(
+                    "RegNo: " + regno + " " +
+                    "Name: " + name + " " +
+                    "Class: " + class_ + " " +
+                    "Section: " + section + " " +
+                    "Contact: " + contact + " " +
+                    "Address: " + address
+            );
+        }
+        System.out.println("************\n");
+        
+    }
 }
